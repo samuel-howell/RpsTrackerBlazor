fbcbc0a2a1018cb3ac840a40a4061e3268417ffc
User <User@DESKTOP-OFNSVN2>
2023-01-03 16:13:55 -0500

    Add project files.

---
+++ RPS.Web.Server/Components/Dashboard/IssueChart.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using RPS.Core.Models.Dto;
+using RPS.Data;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace RPS.Web.Server.Components.Dashboard
+{
+    public partial class IssueChart : ComponentBase
+    {
+        public object[] Categories { get; set; }
+        public List<object> ItemsOpenByMonth { get; set; }
+        public List<object> ItemsClosedByMonth { get; set; }
+
+        [Parameter]
+        public PtDashboardFilter Filter { get; set; }
+
+        [Inject]
+        public IPtDashboardRepository RpsDashData { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            ItemsOpenByMonth = new List<object>();
+            ItemsClosedByMonth= new List<object>();
+            
+            var filteredIssues = RpsDashData.GetFilteredIssues(Filter);
+            filteredIssues.MonthItems.ForEach(i =>
+            {
+                ItemsOpenByMonth.Add(i.Open.Count);
+                ItemsClosedByMonth.Add(i.Closed.Count);
+            });
+
+            Categories = filteredIssues.Categories.Select(i => (object)i).ToArray();
+        }
+    }
+}
---
+++ RPS.Web.Server/Controllers/UploadController.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlazorApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UploadController : Controller
+    {
+        public IWebHostEnvironment HostingEnvironment { get; set; }
+
+        public UploadController(IWebHostEnvironment hostingEnvironment)
+        {
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(IFormFile files) // must match SaveField
+        {
+            if (files != null)
+            {
+                try
+                {
+                    // save to wwwroot - Blazor Server only
+                    var saveLocation = Path.Combine(HostingEnvironment.WebRootPath, files.FileName);
+                    // save to project root - Blazor Server or WebAssembly
+                    //var saveLocation = Path.Combine(HostingEnvironment.ContentRootPath, files.FileName);
+
+                    using (var fileStream = new FileStream(saveLocation, FileMode.Create))
+                    {
+                        await files.CopyToAsync(fileStream);
+                    }
+                }
+                catch
+                {
+                    Response.StatusCode = 500;
+                    await Response.WriteAsync("Upload failed.");
+                }
+            }
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Remove(string files) // must match RemoveField
+        {
+            if (files != null)
+            {
+                try
+                {
+                    // delete from wwwroot - Blazor Server only
+                    var fileLocation = Path.Combine(HostingEnvironment.WebRootPath, files);
+                    // delete from project root - Blazor Server or WebAssembly
+                    //var fileLocation = Path.Combine(HostingEnvironment.ContentRootPath, files);
+
+                    if (System.IO.File.Exists(fileLocation))
+                    {
+                        System.IO.File.Delete(fileLocation);
+                    }
+                }
+                catch
+                {
+                    Response.StatusCode = 500;
+                    Response.WriteAsync("File deletion failed.");
+                }
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
